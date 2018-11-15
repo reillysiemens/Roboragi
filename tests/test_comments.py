0f13c9a50384a3794140e760f92b1fec5cf93a81
Reilly Tucker Siemens <reilly@tuckersiemens.com>
2018-11-14 19:21:16 -0800

    Add multiple comment parsing test

--- tests/test_comments.py
+++ tests/test_comments.py
@@ -41,3 +41,68 @@ from roboragi.comment import parse, Request, Tag
 ])
 def test_comment_parsing(comment: str, requests: List[Request]) -> None:
     assert list(parse(comment)) == requests
+
+
+@pytest.mark.parametrize('comment,requests', [
+    pytest.param(
+        '{anime0} {anime1}',
+        [
+            Request(tag=Tag.ANIME, body='anime0'),
+            Request(tag=Tag.ANIME, body='anime1')
+        ]
+    ),
+    pytest.param(
+        '{{anime0}} {{anime1}}',
+        [
+            Request(tag=Tag.ANIME, body='anime0', expanded=True),
+            Request(tag=Tag.ANIME, body='anime1', expanded=True)
+        ]
+    ),
+    pytest.param(
+        '<manga0> <manga1>',
+        [
+            Request(tag=Tag.MANGA, body='manga0'),
+            Request(tag=Tag.MANGA, body='manga1')
+        ]
+    ),
+    pytest.param(
+        '<<manga0>> <<manga1>>',
+        [
+            Request(tag=Tag.MANGA, body='manga0'),
+            Request(tag=Tag.MANGA, body='manga1')
+        ]
+    ),
+    pytest.param(
+        ']light novel0[ ]light novel1[',
+        [
+            Request(tag=Tag.MANGA, body='light novel0'),
+            Request(tag=Tag.MANGA, body='light novel1')
+        ]
+    ),
+    pytest.param(
+        ']]light novel0[[ ]]light novel1[[',
+        [
+            Request(tag=Tag.MANGA, body='light novel0', expanded=True),
+            Request(tag=Tag.MANGA, body='light novel1', expanded=True)
+        ]
+    ),
+    pytest.param(
+        '|visual novel0| |visual novel1|',
+        [
+            Request(tag=Tag.MANGA, body='visual novel0'),
+            Request(tag=Tag.MANGA, body='visual novel1')
+        ]
+    ),
+    pytest.param(
+        '||visual novel0|| ||visual novel1||',
+        [
+            Request(tag=Tag.MANGA, body='visual novel0', expanded=True),
+            Request(tag=Tag.MANGA, body='visual novel1', expanded=True)
+        ]
+    ),
+])
+def test_multiple_comment_parsing(
+    comment: str,
+    requests: List[Request]
+) -> None:
+    assert list(parse(comment)) == requests
